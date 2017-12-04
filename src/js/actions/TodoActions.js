4fb5a42dc120edc511fca4fbab86f38291264331
Kenny <kenny_hung@mail.chungyo.net>
2017-12-04 19:04:42 +0800

    實作新增待辦事項的 flux 動作

--- src/js/actions/TodoActions.js
+++ src/js/actions/TodoActions.js
@@ -4,6 +4,13 @@ class TodoActions {
     constructor() {
         this._store = TodoStore;
     }
+
+    createTodo(todo) {
+        this._store.getDispatcher().dispatch({
+            actionType: this._store.DISPATCHER_CREATE_TODO,
+            todo: todo
+        });
+    }
 }
 
 export default new TodoActions();
--- src/js/store/TodoStore.js
+++ src/js/store/TodoStore.js
@@ -4,6 +4,7 @@ import Todo from "../classes/Todo";
 class TodoStore extends BaseStore {
     constructor() {
         super();
+        this.DISPATCHER_CREATE_TODO = 'CREATE_TODO';
         this._currentTodo = new Todo();
         this._todos = [];
     }
@@ -16,8 +17,16 @@ class TodoStore extends BaseStore {
         return this._todos;
     }
 
+    _appendTodo(todo) {
+        this._todos.push(todo)
+    }
+
     dispatchHandler(action) {
         switch (action.actionType) {
+            case this.DISPATCHER_CREATE_TODO:
+                this._appendTodo(action.todo);
+                break;
+
             //do nothing
             default:
                 break;
