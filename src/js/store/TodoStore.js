f1af2155f2437253a79004f797312894f409e297
Kenny <kenny_hung@mail.chungyo.net>
2017-12-04 19:03:09 +0800

    新增取得當前待辦事項和代辦事項列表至 store 上

--- src/js/store/TodoStore.js
+++ src/js/store/TodoStore.js
@@ -1,8 +1,19 @@
 import BaseStore from "../classes/BaseStore";
+import Todo from "../classes/Todo";
 
 class TodoStore extends BaseStore {
     constructor() {
         super();
+        this._currentTodo = new Todo();
+        this._todos = [];
+    }
+
+    getCurrentTodo() {
+        return this._currentTodo;
+    }
+
+    getList() {
+        return this._todos;
     }
 
     dispatchHandler(action) {
