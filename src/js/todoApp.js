d62409640be74802dd1c23193020d6e19605b9f0
Kenny <kenny_hung@mail.chungyo.net>
2017-12-14 18:18:59 +0800

    新增待辦事項基礎框架

--- src/js/todoApp.js
+++ src/js/todoApp.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-    <div>Hello World!</div>,
+    <section className="todoapp">
+        <div>
+            <header className="header">
+                <h1>todos</h1>
+                {/*TODO create todo input*/}
+            </header>
+            <section className="main">
+                {/*TODO toggle all todos*/}
+                {/*TODO todo list*/}
+            </section>
+            <footer className="footer">
+                {/*TODO todo count*/}
+                {/*TODO todo filters*/}
+                {/*TODO clear complete todos*/}
+            </footer>
+        </div>
+    </section>,
     document.getElementById("todoApp")
 );
