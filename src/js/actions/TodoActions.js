f20db70a8810ec03fd8a954a21b745b951427c87
Kenny <kenny_hung@mail.chungyo.net>
2017-12-27 11:35:18 +0800

    簡化待辦事項動作取得 dispatcher 的方法

--- src/js/actions/TodoActions.js
+++ src/js/actions/TodoActions.js
@@ -3,52 +3,52 @@ import { TodoActionTypes } from "../constants/ActionTypes";
 
 class TodoActions {
     constructor() {
-        this._store = TodoStore;
+        this._dispatcher = TodoStore.getDispatcher();
     }
 
     createTodo(todo) {
-        this._store.getDispatcher().dispatch({
+        this._dispatcher.dispatch({
             actionType: TodoActionTypes.CREATE_TODO,
             todo: todo
         });
     }
 
     updateTodo(todo) {
-        this._store.getDispatcher().dispatch({
+        this._dispatcher.dispatch({
             actionType: TodoActionTypes.UPDATE_TODO,
             todo: todo
         });
     }
 
     destroyTodo(todo) {
-        this._store.getDispatcher().dispatch({
+        this._dispatcher.dispatch({
             actionType: TodoActionTypes.DESTROY_TODO,
             todo: todo
         });
     }
 
     toggleTodoCompleted(todo) {
-        this._store.getDispatcher().dispatch({
+        this._dispatcher.dispatch({
             actionType: TodoActionTypes.TOGGLE_TODO_COMPLETED,
             todo: todo
         });
     }
 
     toggleAllTodoCompleted(isCompleted) {
-        this._store.getDispatcher().dispatch({
+        this._dispatcher.dispatch({
             actionType: TodoActionTypes.TOGGLE_ALL_TODO_COMPLETED,
             isCompleted: isCompleted
         });
     }
 
     clearAllCompletedTodo() {
-        this._store.getDispatcher().dispatch({
+        this._dispatcher.dispatch({
             actionType: TodoActionTypes.CLEAR_ALL_COMPLETED_TODO
         });
     }
 
     setSelectedFilterLabel(label) {
-        this._store.getDispatcher().dispatch({
+        this._dispatcher.dispatch({
             actionType: TodoActionTypes.SET_SELECTED_FILTER_LABEL,
             label: label
         });
